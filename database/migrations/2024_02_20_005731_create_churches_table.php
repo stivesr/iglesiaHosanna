a4dc68388b44d09f514d6c0ee2f39260d0cf1308
Axel Ramirez <axel.ramirez@ucr.ac.cr>
2024-02-19 19:08:37 -0600

    idChurch

--- database/migrations/2024_02_20_005731_create_churches_table.php
+++ database/migrations/2024_02_20_005731_create_churches_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('churches', function (Blueprint $table) {
-            $table->id();
+            $table->id('idChurch');
             $table->timestamps();
         });
     }
