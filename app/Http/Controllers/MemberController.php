c58ab285b2b5abf44e9afd7cd50da204166dcdb8
“stivesr” <gilberthsolisrios@gmail.com>
2024-02-19 18:58:38 -0600

    addChurch

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function read()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+    
+}
--- database/migrations/2024_02_20_005731_create_churches_table.php
+++ database/migrations/2024_02_20_005731_create_churches_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('members', function (Blueprint $table) {
-            $table->id('idMember');
+        Schema::create('churches', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('members');
+        Schema::dropIfExists('churches');
     }
 };
