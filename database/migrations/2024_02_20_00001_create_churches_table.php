f33db87c518e6e2145d86d2148f1902ea129dc3f
“stivesr” <gilberthsolisrios@gmail.com>
2024-02-29 23:01:24 -0600

    Update

--- database/migrations/2024_02_20_00001_create_churches_table.php
+++ database/migrations/2024_02_20_00001_create_churches_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('location')->nullable(false);
             $table->string('phone');
             $table->string('email');
+            $table->string('image');
 
             $table->timestamps();
             
